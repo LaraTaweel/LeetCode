647fe44547b6ac029b87a332c71ffdcc1f44e301
LaraTaweel <117101096+LaraTaweel@users.noreply.github.com>
2025-03-08 22:43:07 +0200

    Time: 0 ms (100.00%) | Memory: 54.3 MB (21.95%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} candies 
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let maxCandies = candies[0];
+    let result=new Array(candies.length)
+
+    for ( let i =1 ; i< candies.length ; i++){
+        if (candies[i]>maxCandies )
+        maxCandies =candies[i];
+    }
+    for ( let i =0 ; i< candies.length ; i++){
+        if( candies[i] + extraCandies >= maxCandies){
+        result[i] = true 
+    }
+    else result [i]= false 
+    }
+    return result
+
+
+
+};
